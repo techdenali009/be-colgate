c5457a29381daf81999a1bf4a63ae5a22188a196
Aleena <aleena.joy@techdenali.com>
2025-01-24 14:37:32 +0530

    remove and get coupons functionality is added

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import { errorResponse } from './utils/response'
 app.use(express.json());
 app.use(cors({
     origin: [
-     'http://localhost:3000',  
+      'http://localhost:3000',  
       "http://localhost:5173"
     ], // Add allowed domains
     credentials: true 
